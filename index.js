87376923bdb18f793018fe96cf1e5b0fae84de77
SpiritByte <69761403+SpiritByte@users.noreply.github.com>
2023-01-17 17:27:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
         // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          content: `👍`
+          content: `Generated successfully! Check your DMs!.`
         }
       });
     }
