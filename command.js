dc870fff037cbdda4093e9dfeef53fbac90a4e0a
SpiritByte <69761403+SpiritByte@users.noreply.github.com>
2023-03-07 08:43:08 -0500

    Delete command.js

--- command.js
+++
@@ -1,8 +0,0 @@
-// authenticates you with the API standard library
-const lib = require('lib')({token: process.env.TOKEN});
-
-lib.discord.users['@0.2.1'].me.status.update({
-  activity_name: `your mom lmfaooo`,
-  activity_type: 'WATCHING',
-  status: 'ONLINE'
-});
