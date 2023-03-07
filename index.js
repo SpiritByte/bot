139a6fb8a62156383abf2d8013eb3427018bf263
SpiritByte <69761403+SpiritByte@users.noreply.github.com>
2023-03-07 18:04:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -49,13 +49,12 @@ function wait(ms){
 app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
    function log(display) {
 	const p = ' has claimed the faucet!'
-	const g = interaction.member.displayName
         let webhook_url = 'https://discord.com/api/webhooks/1082676859063504936/jXVz5xdmLj_f1lso7P15GNwZVIe-9FM4nP6Cli6e3Pip--KmLiP_08hs8-XaGlQwPE_S';
 
         let params = {
             username: 'Faucet Claim Bot',
 	    avatar: 'https://cdn.discordapp.com/attachments/1081352228557832334/1082678653554532432/discord_gambling_server.png',
-            content: display + p,
+            content: toString(display) + p,
         };
 
         return axios({
