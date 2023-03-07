480fa84b4f8aa1a4eb12e92459503347c402cd75
SpiritByte <69761403+SpiritByte@users.noreply.github.com>
2023-03-07 10:21:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
    function log(msg) {
 	 const p = ' has claimed the faucet!'
 	 const g = interaction.member.user.username
-	 const s = Buffer.concat([g,p]);
         let webhook_url = 'https://discord.com/api/webhooks/1082676859063504936/jXVz5xdmLj_f1lso7P15GNwZVIe-9FM4nP6Cli6e3Pip--KmLiP_08hs8-XaGlQwPE_S';
 
         let params = {
