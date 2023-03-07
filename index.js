968ef44e9b7510180cb0a433ba80f627e3d3f14a
SpiritByte <69761403+SpiritByte@users.noreply.github.com>
2023-03-07 08:40:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ function main(sus) {
 	}
 }
 
+// authenticates you with the API standard library
+const lib = require('lib')({token: TOKEN});
+
+lib.discord.users['@0.2.1'].me.status.update({
+  activity_name: `your mom lmfaooo`,
+  activity_type: 'WATCHING',
+  status: 'ONLINE'
+});
 
 app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
   const interaction = req.body;
