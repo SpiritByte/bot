fca09811d14e1eef7f2d2443bda9520b17f40468
Alexander Li <69761403+SpiritByte@users.noreply.github.com>
2024-01-08 22:18:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const GUILD_ID = process.env.GUILD_ID
 const GAME_ID_BSS = (process.env.GAME_ID_BSS).split("A")
 const GAME_ID_OTHER = (process.env.GAME_ID_BSS).split("A")
 
-
-console.log((process.env.GAME_ID_1).split("A"))
-
-
 const axios = require('axios')
 const express = require('express');
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
@@ -46,18 +42,18 @@ function privateservers(interaction) {
     let privateServerLinks = [];
 
     if (chosenGame === 'bss') {
-        for (const code of GAME_ID_BSS) {
-            privateServerLinks.push(`[Private Server](https://www.roblox.com/games/1537690962/Bee-Swarm-Simulator?privateServerLinkCode=${code})`);
+        for (let i = 0; i < GAME_ID_BSS.length; i++) {
+            privateServerLinks.push(`[${i + 1}](https://www.roblox.com/games/1537690962/Bee-Swarm-Simulator?privateServerLinkCode=${GAME_ID_BSS[i]})`);
         }
     } else if (chosenGame === 'other') {
-        // Replace this block with logic for another game option
-        for (const code of GAME_ID_OTHER) {
-            privateServerLinks.push(`[Private Server](https://www.example.com/otherGamePrivateServerLink?privateServerLinkCode=${code})`);
+        for (let i = 0; i < GAME_ID_OTHER.length; i++) {
+            privateServerLinks.push(`[${i + 1}](https://www.example.com/otherGamePrivateServerLink?privateServerLinkCode=${GAME_ID_OTHER[i]})`);
         }
     }
 
-    return privateServerLinks.join('\n'); // Combine the links into a single string separated by newlines
+    return privateServerLinks.join('\n');
 }
+
 
 
 function wait(ms){
